22862d6082e1c3334a0b55d6baef6d7f0cebbba6
famat <contact@famat.me>
2026-09-11 18:32:02 +0200

    vc-funding 0.2.0

--- Casks/vc-funding.rb
+++ Casks/vc-funding.rb
@@ -1,8 +1,8 @@
 cask "vc-funding" do
-  version "0.1.0"
-  sha256 "da9e3f65b9867c25292f1182d092f8d13b8fbb37e8119d00f46b622f242ae038"
+  version "0.2.0"
+  sha256 "16f3fd4985483c8d3d11c4033b19514a741c8d755dbc6857fb5bad5bd1069c5a"
 
-  url "https://github.com/Fran-cois/vc-funding/releases/download/v0.1.0/vc-funding-0.1.0.zip"
+  url "https://github.com/Fran-cois/vc-funding/releases/download/v0.2.0/vc-funding-0.2.0.zip"
   name "vc-funding"
   desc "Menu bar monitor for coding-agent usage"
   homepage "https://github.com/Fran-cois/vc-funding"
